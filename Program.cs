898356fb3bd588db1b141bcd3049c725acb5e858
Crsky <crskycode@foxmail.com>
2022-09-11 08:29:14 +0800

    Add clear mode

--- Program.cs
+++ Program.cs
@@ -11,10 +11,13 @@ namespace GenHxHashList
             if (args.Length < 1)
             {
                 Console.WriteLine("Usage:");
-                Console.WriteLine("  GenHxHashList \"Dump.log\"...");
+                Console.WriteLine("  GenHxHashList Dump.log ...");
+                Console.WriteLine();
+                Console.WriteLine("Clear mode: Clear all duplicate entries.");
+                Console.WriteLine("  GenHxHashList -c Names.lst");
                 Console.WriteLine();
                 Console.WriteLine("Note: Support multi input file,");
-                Console.WriteLine("  output will created in tool directory.");
+                Console.WriteLine("  output will created in current directory.");
                 Console.WriteLine();
                 Console.WriteLine("Press any key to exit.");
                 Console.ReadKey();
@@ -23,6 +26,52 @@ namespace GenHxHashList
 
             var path_map = new Dictionary<string, string>();
             var name_map = new Dictionary<string, string>();
+
+            if (args.Length > 1)
+            {
+                if (args[0] == "-c")
+                {
+                    Console.WriteLine("Clear mode...");
+
+                    using (var stream = File.OpenText(args[1]))
+                    {
+                        while (!stream.EndOfStream)
+                        {
+                            var line = stream.ReadLine();
+
+                            if (line == null || line.Length == 0)
+                                continue;
+
+                            var entry = line.Split(':');
+
+                            if (entry.Length != 2)
+                                continue;
+
+                            if (entry[0].Length == 16)
+                            {
+                                if (entry[1].Length > 0)
+                                {
+                                    path_map[entry[0]] = entry[1];
+                                    continue;
+                                }
+                            }
+                            if (entry[0].Length == 64)
+                            {
+                                if (entry[1].Length > 0)
+                                {
+                                    name_map[entry[0]] = entry[1];
+                                    continue;
+                                }
+                            }
+                        }
+                    }
+
+                    if (path_map.Count > 0 || name_map.Count > 0)
+                    {
+                        goto write_result;
+                    }
+                }
+            }
 
             foreach (var arg in args)
             {
@@ -57,6 +106,8 @@ namespace GenHxHashList
                     }
                 }
             }
+
+write_result:
 
             Console.WriteLine("Write list...");
 
